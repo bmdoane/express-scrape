98928629f557cd8529722552424b223d7dfbc0be
Brian Doane <brianmdoane@gmail.com>
2016-09-09 13:37:34 -0500

    set up basic server and some other project files

--- server.js
+++ server.js
@@ -3,3 +3,39 @@
 const express = require('express')
 const cheerio = require('cheerio')
 const fetch = require('fetch')
+const routes = require('./routes/') // same as ./routes/index.js
+
+// Initialize
+const app = express()
+
+// Configure
+const port = process.env.Port || 3000
+app.set('port', port)
+
+// app.set('view engine', 'pug')
+
+// if (process.env.Node_ENV !== 'production') {
+// 	app.locals.pretty = true
+// }
+
+// app.locals.......
+
+// Middlewares
+
+
+// Routes
+app.get('/', (req, res) =>
+	res.send('<h1>Welcome to MyApp!</h1>')
+)
+
+
+
+// 404 catch and forward to error handler
+// app.use((req, res) => {
+// 	res.render('404')
+// })
+
+// Listen to requests on the provided port and log when available
+app.listen(port, () => {
+	console.log(`Listening on port: ${port}`)
+})
